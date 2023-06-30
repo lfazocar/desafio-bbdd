372c36c7185d58b272acd419dd125733cde4453b
Felipe <lfazocar@users.noreply.github.com>
2023-06-30 19:17:07 -0400

    feat: Created controllers for each model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'documentary_films/index'
+  get 'documentary_films/create'
+  get 'documentary_films/new'
+  get 'series/index'
+  get 'series/create'
+  get 'series/new'
+  get 'movies/index'
+  get 'movies/create'
+  get 'movies/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
